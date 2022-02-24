e6222523de0d6e43f85b9c0a158ff6d580abac4a
gs fage <879756530@qq.com>
2022-02-24 23:38:38 +0800

    88

---
+++ net/http.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func main() {
+
+	http.HandleFunc("/test", handlefunc)
+	http.ListenAndServe("127.0.0.1:8080",nil)
+}
+
+func handlefunc(rs http.ResponseWriter, rq *http.Request) {
+	rq.
+}
